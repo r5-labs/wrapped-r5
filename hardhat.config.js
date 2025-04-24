e76e882a0eecb74283f9146bd1daa182a48da8b4
ZNX <henrique.baronceli@gmail.com>
2025-04-24 12:18:46 +1200

    update compiler version

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,7 @@ require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
 
 module.exports = {
-  solidity: "0.8.0",
+  solidity: "0.8.26",
   networks: {
     mainnet: { url: process.env.MAINNET_RPC_URL, accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [] },
     testnet: { url: process.env.TESTNET_RPC_URL, accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [] }
